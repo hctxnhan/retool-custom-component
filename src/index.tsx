448164576a90482e9d91776310fc75a6274e2777
Thanh Nhan <hctxnhan@gmail.com>
2025-04-08 14:47:58 +0700

    Boilerplate

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,61 @@
 import React from 'react'
 import { type FC } from 'react'
-
+import './output.css'
 import { Retool } from '@tryretool/custom-component-support'
+import { Button } from './components/ui/button'
 
-export const HelloWorld: FC = () => {
+export const NhanTestComponent: FC = () => {
   const [name, _setName] = Retool.useStateString({
     name: 'name'
   })
 
+  const [theme, _setTheme] = Retool.useStateObject({
+    name: 'theme'
+  })
+
   return (
     <div>
-      <div>Hello {name}!</div>
+      <WrapperComponent cssVariables={theme}>
+        <Button className="w-full h-full" variant={'default'}>
+          Hello {name}
+        </Button>
+      </WrapperComponent>
+    </div>
+  )
+}
+
+export const BBBBB: FC = () => {
+  const [name, _setName] = Retool.useStateString({
+    name: 'name'
+  })
+
+  const [theme, _setTheme] = Retool.useStateObject({
+    name: 'theme'
+  })
+
+  return (
+    <div>
+      <WrapperComponent cssVariables={theme}>text</WrapperComponent>
+    </div>
+  )
+}
+
+const WrapperComponent = ({
+  cssVariables,
+  children
+}: {
+  cssVariables: Record<string, string>
+  children: React.ReactNode
+}) => {
+  return (
+    <div
+      style={{
+        ...Object.fromEntries(
+          Object.entries(cssVariables).map(([k, v]) => [k, v])
+        )
+      }}
+    >
+      {children}
     </div>
   )
 }
